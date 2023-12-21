05e03126bb4f17cdfc703d9ae18df918a4925c9a
diego.bustamante@utec.edu.pe <91268999+NewRenTL@users.noreply.github.com>
2023-12-21 11:14:41 -0500

    I pushed 2 exercises to local repository

--- src/Exercises/Exercise1_2.java
+++ src/Exercises/Exercise1_2.java
@@ -1,4 +1,17 @@
 package Exercises;
 
+import java.util.Scanner;
+import java.lang.Math;
 public class Exercise1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float n1 = sc.nextFloat();
+        float n2 = sc.nextFloat();
+        System.out.println("Suma: "+(n1+n2));
+        System.out.println("Resta: "+(n1-n2));
+        System.out.println("Producto: "+(n1*n2));
+        System.out.println("Media: "+((n1+n2)/2));
+        System.out.print("Distancia: "+Math.abs(n1-n2));
+
+    }
 }
---
+++ src/Exercises/Exercise1_3.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Exercise1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float PI = 3.14f;
+        double radio = sc.nextDouble();
+        System.out.println("El area de un circulo de radio "+radio+" es:"+PI*Math.pow(radio,2));
+
+    }
+}
